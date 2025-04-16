47269cdc8104656c00dcf30cfe17a5dd387ea926
Larry <80271961+L-baker-583@users.noreply.github.com>
2025-04-15 18:23:59 -0600

    Initial outline

--- temp.py
+++ temp.py
@@ -4,3 +4,18 @@ This is a test python file for the DIP final project 25 github repository.
 It is 4/11/25, 13:43 pm.
 """
 
+# Library Imports
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Importing images from dataset
+
+# Fourier transform of subset(?) of images
+
+# Frequency filtering of images
+
+# Histogram visualization of a subset(?) of images
+
+# Histogram modification(?) of images
+
+# TBD Classification method
